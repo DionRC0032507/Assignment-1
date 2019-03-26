91dacc4e884fe5a8ec57226d84bc0849ee511f64
rc0032507 <rc0032507@CRO-24-06>
2019-03-26 10:39:14 +0000

    Add project files.

---
+++ Assignment 1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double resistor1 = double.Parse(textBox1.Text);
+            double resistor2 = double.Parse(textBox2.Text);
+            double resistor3 = double.Parse(textBox3.Text);
+            double resistor4 = double.Parse(textBox4.Text);
+
+            double TotalResistance = resistor1 + resistor2 + resistor3 + resistor4;
+            MessageBox.Show("Total resistance =" + TotalResistance.ToString("0.00") + "ohms");
+        }
+    }
+}
